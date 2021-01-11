e2457239d7129de89b72c52fa5f1feb19d0e0761
Edward Robinson <edward.robinson@teralogics.com>
2021-01-11 09:45:50 -0500

    implemented basic logging capabilities

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ log/logger.go
@@ -0,0 +1,61 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+// LogEvent stores messages to log later, from our standard interface
+type LogEvent struct {
+	id      int
+	message string
+}
+
+// MainLogger enforces specific log messag formats
+type MainLogger struct {
+	*logrus.Logger
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
+// NewLogger intializes the standard logger
+func NewLogger() *MainLogger {
+	f, err := os.OpenFile("logrus.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+	var baseLogger = logrus.New()
+	var standardLogger = &MainLogger{baseLogger}
+	standardLogger.Formatter = &logrus.JSONFormatter{}
+	mw := io.MultiWriter(os.Stdout, f)
+	standardLogger.SetOutput(mw)
+	return standardLogger
+}
+
+// Variables to store our log messages as new Events
+var (
+	invalidArgMessage      = LogEvent{1, "Invalid arg: %s"}
+	invalidArgValueMessage = LogEvent{2, "Invalid value for argument: %s: %v"}
+	missingArgMessage      = LogEvent{3, "Missing arg: %s"}
+)
+
+// InvalidArg error message
+func (l *MainLogger) InvalidArg(argumentName string) {
+	l.Errorf(invalidArgMessage.message, argumentName)
+}
+
+// InvalidArgValue error message
+func (l *MainLogger) InvalidArgValue(argumentName string, argumentValue string) {
+	l.Errorf(invalidArgValueMessage.message, argumentName, argumentValue)
+}
+
+// MissingArg error message
+func (l *MainLogger) MissingArg(argumentName string) {
+	l.Errorf(missingArgMessage.message, argumentName)
+}
